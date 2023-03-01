6492414faa531a4974202d566a6fd5a98e261517
KyouShou <j_123321@hotmail.com>
2023-03-02 04:31:10 +0800

    做出判斷spare和strike的簡易功能，並進行簡單的單元測試
    做出判斷spare和strike的簡易功能，並進行簡單的單元測試

--- TDD.Bowling.Kata.Test/GameTests.cs
+++ TDD.Bowling.Kata.Test/GameTests.cs
@@ -12,6 +12,12 @@ namespace TDD.Bowling.Kata.Test
             _game = new Game();
         }
 
+        [TearDown]
+        public void EndGame()
+        {
+            _game = null;
+        }
+
         [Test]
         public void Score_RollOneTimeAndMiss_ReturnZero()
         {
@@ -26,7 +32,25 @@ namespace TDD.Bowling.Kata.Test
             {
                 _game.Roll(1);
             }
-            Assert.AreEqual(_game.Score(), 10);
+            Assert.AreEqual(10 , _game.Score());
+        }
+
+        [Test]
+        public void Score_TestSpare_RollFiveFiveThree_ReturnSixteen()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(3);
+            Assert.AreEqual(16 , _game.Score());
+        }
+
+        [Test]
+        public void Score_TestStrike_RollTenSixOne_ReturnTwentyFour()
+        {
+            _game.Roll(10);
+            _game.Roll(6);
+            _game.Roll(1);
+            Assert.AreEqual(24, _game.Score());
         }
     }
 }
--- TDD.Bowling.Kata/Game.cs
+++ TDD.Bowling.Kata/Game.cs
@@ -9,10 +9,14 @@ namespace TDD.Bowling.Kata
     public class Game
     {
         private int _score;
+        private int[] _rollRecords;
+        private int _currentRolls;
 
         public Game()
         {
             _score = 0;
+            _rollRecords = new int[21];
+            _currentRolls = 0;
         }
 
         /// <summary>
@@ -21,11 +25,60 @@ namespace TDD.Bowling.Kata
         /// <param name="pins">擊倒幾個瓶子</param>
         public void Roll(int pins)
         {
-            _score += pins;
+            _rollRecords[_currentRolls++] = pins;
+            if (pins == 10)
+                _currentRolls++;
         }
         public int Score()
         {
+            for (int round = 1; round < 11; round++)
+            {
+                int firstPinIndexInRound = GetFirstPinIndexInRound(round);
+                _score += _rollRecords[firstPinIndexInRound];
+                _score += _rollRecords[firstPinIndexInRound + 1];
+
+                if (IsSpare(round))
+                {
+                    _score += _rollRecords[firstPinIndexInRound + 2];
+                }
+
+                if (IsStrike(round))
+                {
+                    _score += _rollRecords[firstPinIndexInRound + 2];
+                    _score += _rollRecords[firstPinIndexInRound + 3];
+                }
+            }
             return _score;
+        }
+
+        private bool IsSpare(int round)
+        {
+            //第一球
+            int firstPinIndexInRound = GetFirstPinIndexInRound(round);
+
+            //第二球
+            int secondPinIndexInRound = firstPinIndexInRound + 1;
+
+            int totalPinsHittedInRound = _rollRecords[firstPinIndexInRound] + _rollRecords[secondPinIndexInRound];
+
+            if (totalPinsHittedInRound == 10 &&
+                !IsStrike(round))
+                return true;
+            else
+                return false;
+        }
+
+        private bool IsStrike(int round)
+        {
+            int firstPinIndexInRound = GetFirstPinIndexInRound(round);
+            if (_rollRecords[firstPinIndexInRound] == 10)
+                return true;
+            else
+                return false;
+        }
+        private int GetFirstPinIndexInRound(int round)
+        {
+            return (round - 1) * 2;
         }
     }
 }
